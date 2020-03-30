0c8624e4c4466529a7631ac8262721a67f327cc7
Elvina Chang <elvina.chang@gmail.com>
2020-03-30 11:41:30 +0100

    Added tdd exercises, sum of natural numbers that divible by 3 and 5.

---
+++ sum_of_naturalNumbers/NaturalNumbers/NaturalNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalNumbers
+{
+
+//    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+    public class NaturalNumber
+    {
+        public int SumOf(int input)
+        {
+            var result = 0;
+            for (int i = 0; i < input; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ sum_of_naturalNumbers/UnitTestProject1/NaturalNumberSumTests.cs
@@ -0,0 +1,42 @@
+﻿using NaturalNumbers;
+using NUnit.Framework;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class NaturalNumberSum
+    {
+        private NaturalNumber naturalNumber;
+
+        [SetUp]
+        public void Setup()
+        {
+            naturalNumber = new NaturalNumber();
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(3, 3)]
+        [TestCase(5, 8)]
+        [TestCase(10, 23)]
+        public void SumOf_When_input_returns_SumOf(int input, int exacted)
+        {
+            var response = naturalNumber.SumOf(input);
+
+            // Assert
+            Assert.AreEqual(response, exacted);
+        }
+
+        [Test]
+        public void SumOf_When_input_1000_returns_SumOf()
+        {
+            var response = naturalNumber.SumOf(1000);
+
+            // Assert
+            Assert.Greater(response, 1000);
+
+            Console.Write(String.Format("Sum of 1000 is {0}", response));
+        }
+
+    }
+}
